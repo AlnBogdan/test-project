67758fa406febd1c71727887a868d13f281aea29
AlnBogdan <bohdanalenad@gmail.com>
2025-04-15 14:52:44 +0300

    first commit

---
+++ src/lib/components/IncrementButton.svelte
@@ -0,0 +1,14 @@
+<script>
+    let counter = 0;
+
+    function increment() {
+        counter += 1;
+    }
+</script>
+
+<button
+    class="bg-blue-500 text-white p-2 rounded-lg mt-4 block mx-auto"
+    on:click={increment}
+>
+    Increment {counter}
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import IncrementButton from "$lib/components/IncrementButton.svelte";
+</script>
+
+<h1 class="text-red-500 text-4xl p-4 text-center mt-24">Hi I'm Alena!</h1>
+
+<IncrementButton />
+
+<a href="/portfolio" class="text-green-300 text-xl">Link to portfolio</a>
